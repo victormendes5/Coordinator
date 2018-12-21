d3d2f98d2b801478daab90394b952697ae793280
componentesdocliente <componentesdocliente@stone.com.br>
2018-12-21 15:10:49 -0200

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by João Mendes | Stone on 21/12/18.
+//  Copyright © 2018 João Mendes | Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
